1d4b7d5be64e21649a100a1b5d3aafd924995d2c
gavinmorrow <gavinfmorrow@gmail.com>
2021-11-30 12:23:24 -0500

    Create homepage

---
+++ web/script.js
@@ -0,0 +1,4 @@
+import _NavComponents from "./modules/NavComponents.js";
+import _ChatIcon from "./components/ChatIcon.js";
+import ChatListElement from "./components/ChatListElement.js";
+document.getElementById("chatlist").appendChild(new ChatListElement("Test", "chatIcon.svg"));
