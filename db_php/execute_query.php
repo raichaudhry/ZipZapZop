79de5cf11393ff0c6720873cb401c19ffe6d3239
gavinmorrow <gavinfmorrow@gmail.com>
2021-12-29 17:36:06 -0500

    Update files

--- db_php/execute_query.php
+++
@@ -1,7 +0,0 @@
-<?php
-	function execute_query(String $query = "", Array $vars = []): ?String {
-		# Run SQL query…
-		# Return the response (or null if it failed)
-		return "";
-	}
-?>
--- web/script.js
+++
@@ -1,7 +0,0 @@
-import _NavComponents from "./modules/NavComponents.js";
-import ChatListElement from "./components/ChatListElement/main.js";
-
-const chatList = document.getElementById("chat-list");
-
-let testElem = new ChatListElement("Eloquent JavaScript", "https://eloquentjavascript.net/favicon.ico");
-chatList.appendChild(testElem);
