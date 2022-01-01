060f77d3952d3797524cbc029e23224338601484
gavinmorrow <gavinfmorrow@gmail.com>
2022-01-01 17:44:05 -0500

    Update chat.js
    Phase out `removeQuotes`

--- backend/chat.js
+++ backend/chat.js
@@ -1,11 +1,9 @@
-const removeQuotes = require("./functions/removeQuotes");
 const authChat = require("./functions/authChat");
 
 const { Pool } = require("pg");
 const pool = new Pool({ port: 5433 });
 
 const express = require("express");
-const auth = require("./functions/auth");
 const router = express.Router();
 
 router.use(express.json());
@@ -14,15 +12,17 @@ router.use(express.text());
 
 router.get("/db/chats/:cuid/:uuid/:pass/:key", async (req, res) => {
 	// cuid = chat uid, uuid = user uid
-	let { cuid: cuid_, uuid: uuid_, pass: pass_, key: key_ } = req.params;
+	let { cuid, uuid, pass, key } = req.params;
 	const client = await pool.connect();
 	try {
-		const { cuid_: cuid, uuid_: uuid, pass_: pass, key_: key } = removeQuotes({ cuid_, uuid_, pass_, key_ });
+		cuid = cuid.replace("'", '"');
+		uuid = uuid.replace("'", '"');
+		pass = pass.replace("'", '"');
+		key = key.replace('"', "'");
 
 		// Check user authorization
 		if (!authChat(cuid, uuid, pass)) res.status(403).send("");
 		else {
-			const query = await client.query(`SELECT ${key === "*" ? "*" : `'${key}'`} FROM chats WHERE uid='${cuid}'`);
 			res.send(JSON.stringify(query.rows[0]));
 		}
 	} finally {
