44408d919c235c4ba0420410c59444302bf9ef53
gavinmorrow <gavinfmorrow@gmail.com>
2022-01-01 13:14:34 -0500

    Update components.js
    Update exports

--- public/modules/components.js
+++ public/modules/components.js
@@ -2,3 +2,4 @@ import ButtonIcon from "../components/ButtonIcon/main.js";
 import TopNav from "../components/TopNav/main.js";
 import ChatListElement from "../components/ChatListElement/main.js";
 import ChatView from "../components/ChatView/main.js";
+export default { ButtonIcon, TopNav, ChatListElement, ChatView };
