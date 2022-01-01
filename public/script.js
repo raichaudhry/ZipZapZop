9f666877cfd4cc93f0b8d5dcf09d72b935343b79
gavinmorrow <gavinfmorrow@gmail.com>
2022-01-01 13:15:19 -0500

    Fix imports

--- public/script.js
+++ public/script.js
@@ -1,9 +1,13 @@
 import "./modules/components.js";
 import db from "./modules/database.js";
+import components from "./modules/components.js";
 import db from "./modules/db/database.js";
 import Cookie from "./modules/Cookie.js";
 import auth from "./modules/auth.js";
 import { decrypt } from "./modules/encryptor.js";
+import auth from "./modules/db/auth.js";
+import { encrypt, decrypt } from "./modules/encryptor.js";
+import user from "./modules/db/user.js";
 
 const login = () => /*location.href = "./login"*/ undefined;
 
