7c066976b4bf6f5e8cb1ceb86b0dbb48375354bb
gavinmorrow <gavinfmorrow@gmail.com>
2022-01-19 18:49:00 -0500

    Add comments

--- public/login/login.js
+++ public/login/login.js
@@ -1,6 +1,12 @@
 import Cookie from "../modules/Cookie.js";
 import auth from "../modules/db/auth.js";
 
+/**
+ * Sets the cookies to log a user in.
+ * @param {String} username The username of the user.
+ * @param {String} password The password of the user.
+ * @returns {Boolean} If the user can't be logged in, it will return false. If the user was logged in, it will return true.
+ */
 const login = async (username, password) => {
 	if (await auth(username, password, true)) {
 		Cookie.set(
