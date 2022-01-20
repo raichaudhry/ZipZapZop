3463deefb714ab3e262043c69f87cd86f90a8278
gavinmorrow <gavinfmorrow@gmail.com>
2022-01-19 19:14:30 -0500

    Error handling

--- public/script.js
+++ public/script.js
@@ -5,47 +5,53 @@ import { encrypt, decrypt } from "./modules/encryptor.js";
 import user from "./modules/db/user.js";
 import ChatView from "./components/ChatView/main.js";
 
-const cookies = {};
-cookies.username = Cookie.get("username");
-cookies.password = Cookie.get("password");
+try {
+	const cookies = {};
+	cookies.username = Cookie.get("username");
+	cookies.password = Cookie.get("password");
 
-const login = async () => {
-	location.replace("./login");
-};
+	const login = async () => {
+		location.replace("./login");
+	};
 
-const main = async () => {
-	const username = cookies.username.value;
-	const password = cookies.password.value;
+	const main = async () => {
+		const username = cookies.username.value;
+		const password = cookies.password.value;
 
-	cookies.uuid = Cookie.get("uuid") || Cookie.set({ name: "uuid", value: await user(username, password, "uid", undefined, true) })[0];
+		// Get the uuid cookie, or set it.
+		cookies.uuid = Cookie.get("uuid") ?? Cookie.set({ name: "uuid", value: await user(username, password, "uid", undefined, true) })[0];
 
-	const uuid = cookies.uuid.value;
-	const chats = (await user(username, password, "chats", undefined, true)) || [];
+		const uuid = cookies.uuid.value;
+		const chats = (await user(username, password, "chats", undefined, true)) ?? [];
 
-	for (const cuid of chats) {
-		const req = await fetch(`/db/chats/${cuid}/${uuid}/${password}/`);
-		if (req.status < 200 || req.status >= 300) continue;
+		for (const cuid of chats) {
+			const req = await fetch(`/db/chats/${cuid}/${uuid}/${password}/`);
+			if (req.status < 200 || req.status >= 300) continue;
 
-		const data = await req.json();
-		const chatListElem = new components.ChatListElement(data.name, data.uid, "/assets/chatIcon.svg");
+			const data = await req.json();
+			const chatListElem = new components.ChatListElement(data.name, data.uid, "/assets/chatIcon.svg");
 
-		chatListElem.addEventListener(
-			"open-chat",
-			/** @param {CustomEvent} e */ async e => {
-				const chatView = new ChatView(e.detail.cuid, e.detail.name);
-				chatView.setAttribute("open", "");
-				document.body.appendChild(chatView);
-			}
-		);
+			chatListElem.addEventListener(
+				"open-chat",
+				/** @param {CustomEvent} e */ async e => {
+					const chatView = new ChatView(e.detail.cuid, e.detail.name);
+					chatView.setAttribute("open", "");
+					document.body.appendChild(chatView);
+				}
+			);
 
-		document.getElementById("chat-list").appendChild(chatListElem);
-	}
-};
+			document.getElementById("chat-list").appendChild(chatListElem);
+		}
+	};
 
-(async () => {
-	if (cookies.username == undefined || cookies.password == undefined) login();
+	(async () => {
+		if (cookies.username == undefined || cookies.password == undefined) login();
 
-	// Auth
-	if (!(await auth(cookies.username.value, cookies.password.value, true))) login();
-	else main();
-})();
+		// Auth
+		if (!(await auth(cookies.username.value, cookies.password.value, true))) login();
+		else main();
+	})();
+} catch (e) {
+	alert("There was an error. This page will reload automatically. \n${e}");
+	location.reload();
+}
