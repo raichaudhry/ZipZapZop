ab746d3aee61940abc271348702c0b5668d08871
gavinmorrow <gavinfmorrow@gmail.com>
2022-02-07 18:43:45 -0500

    Format code

--- public/modules/db/chat.js
+++ public/modules/db/chat.js
@@ -1,14 +1,30 @@
 import { encrypt } from "../encryptor.js";
-const chat = async (cuid, id, password, key, username = false, username_ = false) => {
+const chat = async (
+	cuid,
+	id,
+	password,
+	key,
+	username = false,
+	username_ = false
+) => {
 	username = username || username_;
 	password = encrypt(password);
 	if (newValue) {
-		const res = await fetch(`/db/write/chats/${cuid}/${username ? "username-" : ""}${id}/${password}/${key}/${newValue}`);
+		const headers = { cuid, password, key, newValue };
+		const res = await fetch(`/db/write/chat`, {
+			headers: username
+				? { username: id, ...headers }
+				: { uuid: uid, ...headers },
+		});
 		if (res.status == 204) return true;
 		else if (res.status == 500) return false;
 		else return null;
 	} else {
-		const res = await fetch(`/db/chats/${cuid}/${username ? "username-" : ""}${id}/${password}/${key}`);
+		const res = await fetch(
+			`/db/chats/${cuid}/${
+				username ? "username-" : ""
+			}${id}/${password}/${key}`
+		);
 		const json = await res.text();
 		try {
 			if (key !== "*") return JSON.parse(json)[key];
