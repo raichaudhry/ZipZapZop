596f98bc92f1a15cef482e77bd82fc14d5a83b0a
gavinmorrow <gavinfmorrow@gmail.com>
2022-02-07 11:29:55 -0500

    Update auth.js
    Fix bugs

--- public/modules/db/auth.js
+++ public/modules/db/auth.js
@@ -1,17 +1,19 @@
 /**
  * Checks a user's credentials to see if they are correct.
- * @param {String} uid The uid or username of the user.
+ * @param {String} id The uid or username of the user.
  * @param {String} password The user's password.
  * @param {Boolean} username **DEPRECATED!!!** Act as if `id` is a username. Defaults to false.
  * @returns {Boolean} True if credentials are correct, false if not.
  */
-const auth = async (uid, password, username = false) => {
+const auth = async (id, password, username = false) => {
 	try {
 		password = encodeURI(password);
 		const res = await fetch(`/auth`, {
-			headers: { uid, password },
+			headers: username
+				? { username: id, password }
+				: { uid: id, password },
 		});
-		switch (res.statusCode) {
+		switch (res.status) {
 			case 200:
 				return true;
 			case 401:
