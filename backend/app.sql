6455ec9859a264490748da886fab46418848938e
gavinmorrow <gavinfmorrow@gmail.com>
2022-02-11 09:16:35 -0500

    Remove test accounts & chat

--- backend/app.sql
+++ backend/app.sql
@@ -6,7 +6,4 @@ CREATE TABLE users (uid UUID PRIMARY KEY DEFAULT gen_random_uuid(), password tex
 
 CREATE TABLE chats (uid uuid PRIMARY KEY DEFAULT gen_random_uuid(), name TEXT NOT NULL, people UUID[] NOT NULL DEFAULT ARRAY[]::UUID[], messages JSON[] NOT NULL DEFAULT ARRAY[]::JSON[]);
 
-INSERT INTO users(password, username, chats) VALUES ('234', 'testuser1', ARRAY[gen_random_uuid()]::UUID[]);
-INSERT INTO chats(uid, name, people) VALUES ((SELECT chats FROM users WHERE username = 'testuser1')[1], 'testchat1', ARRAY(SELECT uid FROM users WHERE username = 'testuser1')::UUID[]);
-
 COMMIT;
