ab00ff9083a97d5089e0e1b2ec131dc78d08ec39
gavinmorrow <gavinfmorrow@gmail.com>
2022-02-14 16:58:36 -0500

    Format code

--- public/login/script.js
+++ public/login/script.js
@@ -1,7 +1,7 @@
 import Cookie from "../modules/Cookie.js";
 import auth from "../modules/db/auth.js";
 import login from "./login.js";
-import { decrypt, encrypt } from "../modules/encryptor.js";
+import { encrypt } from "../modules/encryptor.js";
 
 const cookies = {
 	uid: Cookie.get("uuid"),
@@ -9,9 +9,22 @@ const cookies = {
 	pass: Cookie.get("password"),
 };
 
-const finishLogin = () => location.replace("/" + (new URLSearchParams(document.location.search).get("from") ?? ""));
+const finishLogin = () => {
+	location.replace(
+		"/" + (new URLSearchParams(document.location.search).get("from") ?? "")
+	);
+};
 
-if ((cookies.uid || cookies.username) && cookies.pass && (await auth(cookies.uid?.value ?? cookies.username?.value, cookies.pass.value, cookies.uid?.value ? false : true))) finishLogin(); // Already logged in
+if (
+	(cookies.uid || cookies.username) &&
+	cookies.pass &&
+	(await auth(
+		cookies.uid?.value ?? cookies.username?.value,
+		cookies.pass.value,
+		cookies.uid?.value ? false : true
+	))
+)
+	finishLogin(); // Already logged in
 
 /** @type {HTMLFormElement} */
 const form = document.getElementById("form"),
@@ -23,11 +36,15 @@ const form = document.getElementById("form"),
 form.addEventListener("submit", async () => {
 	const username = usernameInput.value,
 		password = encrypt(passInput.value);
-	if ((await auth(username, password, true)) && (await login(username, password))) {
+	if (
+		(await auth(username, password, true)) &&
+		(await login(username, password))
+	) {
 		document.getElementById("error").innerHTML = "";
 
 		finishLogin();
 	} else {
-		document.getElementById("error").innerHTML = "Incorrect username or password.";
+		document.getElementById("error").innerHTML =
+			"Incorrect username or password.";
 	}
 });
